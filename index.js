2b652f72591b812adca45e9e0efe814be059d3a7
Amy-glitch <ccheetah3012@gmail.com>
2021-01-10 21:56:10 +0200

    4

--- index.js
+++ index.js
@@ -40,36 +40,6 @@ document.ontouchend = function(){
 
 document.ontouchmove = function(e){
 e.preventDefault();
-if (timer) clearInterval(timer);
-
-timer=setInterval(function(){
-     
-    let dX = e.touches[0].clientX -window.innerWidth/2;
-    let dY = e.touches[0].clientY  - window.innerHeight/2;
-        
-    cLeft-=20*Math.sign(dX)*Math.abs(dX)/100;
-    cTop -=20*Math.sign(dY)*Math.abs(dY)/100;
-    canv.style.top = cTop+'px';
-    canv.style.left = cLeft+'px';     
-    
-    let A = Math.atan2(dX,dY) * (180 / Math.PI)+180;
-            ctr+=1;
-            if (A < 45){
-                animateScript(0);
-            } else
-            if (A < 135){
-                animateScript(1);
-            } else
-            if (A < 225){
-                animateScript(2);
-            } else
-            if (A < 315){
-                animateScript(3);
-            } else
-            {
-                animateScript(4);
-            }
-         }, 100); // the above code is executed every 100 ms
 
 
 }
